351da32b10b0541015c6ec01c392ec32c9e3e784
undeadevs <42311213+undeadevs@users.noreply.github.com>
2023-06-16 16:19:09 +0800

    Added comments for ILaporanItem

--- src/com/kelompok1/types/ILaporanItem.java
+++ src/com/kelompok1/types/ILaporanItem.java
@@ -1,4 +1,8 @@
 package com.kelompok1.types;
 
+// Marker Interface (https://en.wikipedia.org/wiki/Marker_interface_pattern)
+// Interface kosong
+// Dalam kasus project ini berguna agar hanya class yang mengimplementasikan interface ini
+// saja yang dapat diletakkan pada ObservableList abstrac class Laporan
 public interface ILaporanItem {
 }
