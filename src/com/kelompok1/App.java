bdfde26613e92cb1ca5ca24e98e10ebb13178ef6
Hairul Irawan <hairulirawann@gmail.com>
2023-06-17 11:28:14 +0800

    First GUI with FXML

--- src/com/kelompok1/App.java
+++ src/com/kelompok1/App.java
@@ -2,19 +2,33 @@ package com.kelompok1;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
     public static Dotenv dotenv;
 
     @Override
-    public void start(Stage stage) throws Exception {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 800, 600);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("./views/Login.fxml"));
+
+        Scene scene = new Scene(root);
+
+        // String css = this.getClass().getResource("MainScene.css").toExternalForm();
+        // scene.getStylesheets().add(css);
+
+        Image icon = new Image(getClass().getResourceAsStream("resources/salary.png"));
+        primaryStage.getIcons().add(icon);
+
+        primaryStage.setTitle("Sistem Informasi Akutansi");
+        primaryStage.sizeToScene();
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/com/kelompok1/controllers/MainController.java
@@ -0,0 +1,30 @@
+package com.kelompok1.controllers;
+
+import com.kelompok1.App;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    private Stage stage;
+    private Scene scene;
+
+    public void switchToMain(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(App.class.getResource("./views/MainScene.fxml"));
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+
+        // String css = this.getClass().getResource("MainScene.css").toExternalForm();
+        // scene.getStylesheets().add(css);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
