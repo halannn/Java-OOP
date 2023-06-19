d4bca7528bf281a565aeb69b46ae6736d17f8b6b
undeadevs <42311213+undeadevs@users.noreply.github.com>
2023-06-20 01:05:06 +0800

    Tweaked BaseController

--- src/com/kelompok1/controllers/BaseController.java
+++ src/com/kelompok1/controllers/BaseController.java
@@ -1,20 +1,12 @@
 package com.kelompok1.controllers;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.Event;
-// import javafx.event.ActionEvent;
-// import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-// import javafx.scene.control.Alert;
-// import javafx.scene.control.Alert.AlertType;
-// import javafx.scene.control.Alert;
-// import javafx.scene.control.Button;
-// import javafx.scene.control.Label;
-// import javafx.scene.control.PasswordField;
-// import javafx.scene.control.TextField;
-// import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,14 +16,18 @@ import com.kelompok1.models.User;
 
 public class BaseController {
     private Stage stage;
-    private User user = null;
+    private ObjectProperty<User> user = new SimpleObjectProperty<>();
 
     public User getUser() {
-        return user;
+        return user.get();
     }
 
     public void setUser(User user) {
-        this.user = user;
+        this.user.set(user);
+    }
+
+    public ObjectProperty<User> userProperty() {
+        return user;
     }
 
     public Stage getStage() {
@@ -42,20 +38,21 @@ public class BaseController {
         this.stage = stage;
     }
 
-    public void switchView(String viewPath) throws IOException {
+    public BaseController switchView(String viewPath) throws IOException {
         FXMLLoader loader = new FXMLLoader(App.class.getResource(viewPath));
         Parent root = loader.load();
 
         Scene scene = new Scene(root);
         BaseController controller = loader.getController();
 
-        // String css = this.getClass().getResource("MainScene.css").toExternalForm();
-        // scene.getStylesheets().add(css);
-
+        String css = App.class.getResource("./resources/main.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        
         stage.setScene(scene);
         controller.setStage(stage);
-        controller.setUser(user);
+        controller.setUser(user.get());
         stage.show();
+        return controller;
     }
 
     public void switchViewFromEvent(Event event) throws IOException {
